152313344fe52fdda2f53e5547073f740357d14c
Jarl Friis <jarl@softace.dk>
2019-08-31 21:31:49 +0200

    Done: Tilføj tænd/sluk sider

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ def cakes():
 def hello(name):
     return render_template('page.html', name=name)
 
+@app.route('/on')
+def tænd():
+    return render_template('tændt.html')
+
+@app.route('/off')
+def sluk():
+    return render_template('slukket.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
