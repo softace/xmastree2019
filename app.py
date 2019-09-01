ec64f5262cdd87f02d852c04cd0d3b4cbb664f7f
Jarl Friis <jarl@softace.dk>
2019-09-01 10:57:30 +0200

    Done: Send kommandoer til microbit via kabel

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import serial
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -16,11 +17,20 @@ def hello(name):
 
 @app.route('/on')
 def tænd():
+    with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as kabel:
+        kabel.write(b"on\n")
     return render_template('tændt.html')
 
 @app.route('/off')
 def sluk():
+    with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as kabel:
+        kabel.write(b"off\n")
     return render_template('slukket.html')
+
+@app.after_request
+def add_header(response):
+    response.cache_control.no_cache = True
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
